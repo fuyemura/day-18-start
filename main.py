ee69d07fcf420054f487819ead7e2d941c8624b6
Flávio Uyemura <fuyemura@gmail.com>
2022-11-21 22:09:25 -0300

    Primeira versão

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
-from turtle import Turtle, Screen
+import turtle as t
+import random
 
-timmy = Turtle()
-timmy.shape("turtle")
-timmy.color("blue")
+tim = t.Turtle()
 
-for i in range(4):
-    timmy.forward(100)
-    timmy.right(90)
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
-screen = Screen()
-screen.exitonclick()
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colours))
+    draw_shape(shape_side_n)
