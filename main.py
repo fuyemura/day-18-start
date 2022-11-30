998429f112bb26adc436fce6cfe2b8654b49c78e
Flávio Uyemura <fuyemura@gmail.com>
2022-11-29 23:00:05 -0300

    Segunda versão

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import turtle as t
 import random
 
-# Challenge 3
 tim = t.Turtle()
+t.colormode(255)
 
 colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+# Challenge 3
+#def draw_shape(num_sides):
+#    angle = 360 / num_sides
+#    for _ in range(num_sides):
+#        tim.forward(100)
+#        tim.right(angle)
+#
+#for shape_side_n in range(3, 11):
+#    tim.color(random.choice(colours))
+#    draw_shape(shape_side_n)
 
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colours))
-    draw_shape(shape_side_n)
+
+# Challenge 4
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r, g, b)
+    return random_color
+
+
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
+
+for _ in range(200):
+    tim.color(random_color())
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
